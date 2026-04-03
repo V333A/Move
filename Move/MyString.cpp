3aaad1d1a8cbc20c9b25a1158bf7e029b1f0b05e
Vadym Alieksieiev <baadir220399@icloud.com>
2026-04-03 16:59:46 +0200

    first commit

--- Move/MyString.cpp
+++ Move/MyString.cpp
@@ -1,42 +1,92 @@
 #include "MyString.h"
+#include <iostream>
 #include <cstring>
 
-MyString::MyString() : str(nullptr)
+int MyString::objectCount = 0;
+
+void MyString::initEmpty(int cap)
 {
-    str = new char[1];
+    if (cap < 1)
+        cap = 1;
+
+    capacity = cap;
+    str = new char[capacity + 1];
     str[0] = '\0';
+    length = 0;
 }
 
-MyString::MyString(const char* s) : str(nullptr)
+void MyString::copyFrom(const char* source)
 {
-    if (s == nullptr)
+    if (source == nullptr)
     {
-        str = new char[1];
-        str[0] = '\0';
+        initEmpty(80);
+        return;
     }
-    else
+
+    length = static_cast<int>(std::strlen(source));
+    capacity = length > 80 ? length : 80;
+    str = new char[capacity + 1];
+    strcpy_s(str, capacity + 1, source);
+}
+
+MyString::MyString()
+{
+    initEmpty(80);
+    ++objectCount;
+}
+
+MyString::MyString(int size)
+{
+    initEmpty(size);
+    ++objectCount;
+}
+
+MyString::MyString(const char* s)
+{
+    copyFrom(s);
+    ++objectCount;
+}
+
+MyString::MyString(const MyString& other)
+{
+    length = other.length;
+    capacity = other.capacity;
+    str = new char[capacity + 1];
+    strcpy_s(str, capacity + 1, other.str);
+    ++objectCount;
+}
+
+MyString::MyString(std::initializer_list<char> il)
+{
+    length = static_cast<int>(il.size());
+    capacity = length > 80 ? length : 80;
+    str = new char[capacity + 1];
+
+    int i = 0;
+    for (char ch : il)
     {
-        size_t len = std::strlen(s);
-        str = new char[len + 1];
-        strcpy_s(str, len + 1, s);
+        str[i++] = ch;
     }
+    str[length] = '\0';
+    ++objectCount;
 }
 
-MyString::MyString(const MyString& other) : str(nullptr)
+MyString::MyString(MyString&& other) noexcept
 {
-    size_t len = std::strlen(other.str);
-    str = new char[len + 1];
-    strcpy_s(str, len + 1, other.str);
-}
+    str = other.str;
+    length = other.length;
+    capacity = other.capacity;
 
-MyString::MyString(MyString&& other) noexcept : str(other.str)
-{
     other.str = nullptr;
+    other.length = 0;
+    other.capacity = 0;
+    ++objectCount;
 }
 
 MyString::~MyString()
 {
     delete[] str;
+    --objectCount;
 }
 
 MyString& MyString::operator=(const MyString& other)
@@ -45,9 +95,10 @@ MyString& MyString::operator=(const MyString& other)
     {
         delete[] str;
 
-        size_t len = std::strlen(other.str);
-        str = new char[len + 1];
-        strcpy_s(str, len + 1, other.str);
+        length = other.length;
+        capacity = other.capacity;
+        str = new char[capacity + 1];
+        strcpy_s(str, capacity + 1, other.str);
     }
     return *this;
 }
@@ -59,14 +110,128 @@ MyString& MyString::operator=(MyString&& other) noexcept
         delete[] str;
 
         str = other.str;
+        length = other.length;
+        capacity = other.capacity;
+
         other.str = nullptr;
+        other.length = 0;
+        other.capacity = 0;
     }
     return *this;
 }
 
-const char* MyString::GetStr() const
+void MyString::Print() const
+{
+    std::cout << str;
+}
+
+void MyString::MyStrcpy(MyString& obj)
+{
+    if (this == &obj)
+        return;
+
+    delete[] str;
+
+    length = obj.length;
+    capacity = obj.capacity;
+    str = new char[capacity + 1];
+    strcpy_s(str, capacity + 1, obj.str);
+}
+
+bool MyString::MyFindStr(const char* substr) const
+{
+    if (substr == nullptr)
+        return false;
+
+    return std::strstr(str, substr) != nullptr;
+}
+
+int MyString::MyFindChr(char c) const
+{
+    for (int i = 0; i < length; ++i)
+    {
+        if (str[i] == c)
+            return i;
+    }
+    return -1;
+}
+
+int MyString::MyStrLen() const
+{
+    return length;
+}
+
+int MyString::GetCapacity() const
+{
+    return capacity;
+}
+
+const char* MyString::c_str() const
 {
     return str;
+}
+
+MyString MyString::operator+(const MyString& other) const
+{
+    int newLen = length + other.length;
+    MyString temp(newLen);
+
+    delete[] temp.str;
+    temp.capacity = newLen > 80 ? newLen : 80;
+    temp.str = new char[temp.capacity + 1];
+
+    strcpy_s(temp.str, temp.capacity + 1, str);
+    strcat_s(temp.str, temp.capacity + 1, other.str);
+    temp.length = newLen;
+
+    return temp;
+}
+
+MyString& MyString::operator+=(const MyString& other)
+{
+    int newLen = length + other.length;
+    int newCap = newLen > capacity ? newLen : capacity;
+
+    char* newStr = new char[newCap + 1];
+    strcpy_s(newStr, newCap + 1, str);
+    strcat_s(newStr, newCap + 1, other.str);
+
+    delete[] str;
+    str = newStr;
+    length = newLen;
+    capacity = newCap;
+
+    return *this;
+}
+
+char& MyString::operator[](int index)
+{
+    return str[index];
+}
+
+const char& MyString::operator[](int index) const
+{
+    return str[index];
+}
+
+bool MyString::operator>(const MyString& other) const
+{
+    return std::strcmp(str, other.str) > 0;
+}
+
+bool MyString::operator<(const MyString& other) const
+{
+    return std::strcmp(str, other.str) < 0;
+}
+
+bool MyString::operator==(const MyString& other) const
+{
+    return std::strcmp(str, other.str) == 0;
+}
+
+int MyString::GetObjectCount()
+{
+    return objectCount;
 }
 
 std::ostream& operator<<(std::ostream& out, const MyString& obj)
@@ -81,9 +246,10 @@ std::istream& operator>>(std::istream& in, MyString& obj)
     in.getline(buffer, 1000);
 
     delete[] obj.str;
-    size_t len = std::strlen(buffer);
-    obj.str = new char[len + 1];
-    strcpy_s(obj.str, len + 1, buffer);
+    obj.length = static_cast<int>(std::strlen(buffer));
+    obj.capacity = obj.length > 80 ? obj.length : 80;
+    obj.str = new char[obj.capacity + 1];
+    strcpy_s(obj.str, obj.capacity + 1, buffer);
 
     return in;
 }
--- Move/MyString.h
+++ Move/MyString.h
@@ -1,22 +1,52 @@
 #pragma once
 #include <iostream>
+#include <initializer_list>
 
 class MyString
 {
 private:
     char* str;
+    int length;
+    int capacity;
+
+    static int objectCount;
+
+    void initEmpty(int cap = 80);
+    void copyFrom(const char* source);
 
 public:
-    MyString();
-    explicit MyString(const char* s);
-    MyString(const MyString& other);
-    MyString(MyString&& other) noexcept;
+    MyString();                              // по умолчанию: строка длиной 80 символов
+    explicit MyString(int size);             // произвольный размер
+    MyString(const char* s);                 // от const char*
+    MyString(const MyString& other);         // copy constructor
+    MyString(std::initializer_list<char> il);// {'a','b','c'}
+    MyString(MyString&& other) noexcept;     // move constructor
     ~MyString();
 
     MyString& operator=(const MyString& other);
     MyString& operator=(MyString&& other) noexcept;
 
-    const char* GetStr() const;
+    void Print() const;
+
+    void MyStrcpy(MyString& obj);            // копирование с затиранием прошлого значения
+    bool MyFindStr(const char* substr) const;
+    int MyFindChr(char c) const;
+    int MyStrLen() const;
+    int GetCapacity() const;
+
+    const char* c_str() const;
+
+    MyString operator+(const MyString& other) const;
+    MyString& operator+=(const MyString& other);
+
+    char& operator[](int index);
+    const char& operator[](int index) const;
+
+    bool operator>(const MyString& other) const;
+    bool operator<(const MyString& other) const;
+    bool operator==(const MyString& other) const;
+
+    static int GetObjectCount();
 
     friend std::ostream& operator<<(std::ostream& out, const MyString& obj);
     friend std::istream& operator>>(std::istream& in, MyString& obj);
